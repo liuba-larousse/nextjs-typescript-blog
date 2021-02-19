0dd53c0f0da5caa4d248c8d07927e0deb74bae31
liuba-larousse <liubovkapitulskaya@gmail.com>
2021-02-18 19:48:39 -0600

    appprops

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }, AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
