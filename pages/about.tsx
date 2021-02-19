b778089be2549aa82e39629423b3ca21dc2b9521
liuba-larousse <liubovkapitulskaya@gmail.com>
2021-02-18 19:27:17 -0600

    image add to post

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import { Article } from '../components/Article';
+
+export default function About() {
+  return (
+    <Article>
+      <h1>About this blog</h1>
+      <p>This is the best blog ever</p>
+    </Article>
+  );
+}
